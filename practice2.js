1ab58492b31ba71d5596af4b13a069c908875de5
Harshita7317 <118048036+Harshita7317@users.noreply.github.com>
2023-04-21 22:38:55 +0530

    Add files via upload

--- practice2.js
+++ practice2.js
@@ -35,6 +35,64 @@ app.get("/api/getmovies", async (req, res) => {
   }
 });
 
+app.post("/api/friendsdata", async (req, res) => {
+  try {
+    console.log(req.body);
+    const friendsobject = {
+      schoolfriend: req.body.schooldaysfriend,
+      coachingfriend: req.body.coachingdaysfriend,
+      hostelfriend: req.body.hosteldaysfriend,
+      collegefriend: req.body.collegedaysfriend,
+      societyfriend: req.body.societydaysfriend,
+    };
+    console.log(friendsobject);
+    const friend = await FRIENDS_MODELS(friendsobject);
+    await friend.save();
+    return res.json({ success: true, message: "data saved successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ success: false, message: error.message });
+  }
+});
+app.get("/api/getfriendsdata", async (req, res) => {
+  try {
+    const Friendsdata = await FRIENDS_MODELS.find();
+    return res.json({ success: true, data: Friendsdata });
+  } catch (error) {
+    console.log(error);
+    return res.json({ sucess: false, message: error.message });
+  }
+});
+//3rd
+app.post("/api/resultsdata", async (req, res) => {
+  try {
+    console.log(req.body);
+    const resultobject = {
+      name: req.body.username,
+      class: req.body.userclass,
+      rollno: req.body.userrollno,
+      physicsmarks: req.body.userphymarks,
+      chemistrymarks: req.body.userchemmarks,
+      mathsmarks: req.body.usermathsmarks,
+    };
+    console.log(resultobject);
+    const result = await RESULT_MODELS(resultobject);
+    await result.save();
+    return res.json({ success: true, message: "data saved" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ success: true, message: error.message });
+  }
+});
+app.get("/api/getresultdata", async (req, res) => {
+  try {
+    const Resultsdata = await RESULT_MODELS.find();
+    return res.json({ success: true, data: Resultsdata });
+  } catch (error) {
+    console.log(error);
+    return res.json({ sucess: false, message: error.message });
+  }
+});
 connectDatabase();
 
 const PORT = 8000;
